93867b96a385be6b68c78bd04ee98d7ada87ce8c
Guilherme Yoshida Teixeira <contact@gyt.app>
2023-01-26 19:41:38 -0300

    :sparkles: isset on __get if

--- src/Core/Domain/Entity/Traits/MagicMethodTrait.php
+++ src/Core/Domain/Entity/Traits/MagicMethodTrait.php
@@ -8,7 +8,7 @@ trait MagicMethodTrait
 {
     public function __get($property)
     {
-        if ($this->{$property}) return $this->{$property};
+        if (isset($this->{$property})) return $this->{$property};
         
         $className = get_class($this);
         throw new Exception("Property {$property} not found in class {$className}");
