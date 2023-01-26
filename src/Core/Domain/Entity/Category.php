a91714eff4d469e6be561e17e390595689a44094
Guilherme Yoshida Teixeira <contact@gyt.app>
2023-01-26 19:31:46 -0300

    :heavy_check_mark: auto Category __constructor

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -10,8 +10,8 @@ class Category
 
     public function __construct(
         protected string $id = '',
-        protected string $name,
-        protected string $description,
+        protected string $name = '',
+        protected string $description = '',
         protected bool $isActive = true,
     )
     {
--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -10,7 +10,6 @@ class CategoryUnitTest extends TestCase
     public function testAttributes()
     {
         $category = new Category(
-            id: '1',
             name: 'New Cat',
             description: 'New desc',
             isActive: true
@@ -19,6 +18,5 @@ class CategoryUnitTest extends TestCase
         $this->assertEquals('New Cat', $category->name);
         $this->assertEquals('New desc', $category->description);
         $this->assertEquals(true, $category->isActive);
-
     }
 }
